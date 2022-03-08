7c8e2bbe803018db7a3cb0ea3a2f5dfb0d134c09
bekboloffisial <tbkg1558@gmail.com>
2022-03-08 17:46:19 +0600

    responsives

--- js/script.js
+++ js/script.js
@@ -2,4 +2,20 @@ $(".slider") .slick({
     slidesToShow: 3,
     arrows: true,
     dots: true,
+    responsive: [
+        {
+            breakpoint: 1140,
+            settings: {
+                slidesToShow: 2,
+                arrows: false,
+            }
+        },
+        {
+            breakpoint: 1040,
+            settings: {
+                slidesToShow: 1,
+                arrows: false,
+            }
+        }
+    ]
 })
